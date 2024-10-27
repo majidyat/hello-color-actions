558e24892c6acc26b42a4002149eb3b68aeaf960
majidyat <majidyat@gmail.com>
2024-10-28 00:19:15 +0300

    update

--- hello.test.js
+++ hello.test.js
@@ -1,6 +1,6 @@
 const hello = require("./hello");
 describe("My hello", () => {
   test("works", () => {
-    expect(hello.hello()).toEqual("Hello World from Suadi Arabia!");
+    expect(hello.hello()).toEqual("Hello World from Saudi Arabia!");
   });
 });
